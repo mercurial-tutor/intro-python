4d3b1b842a65f8fe2459da9b850ca0b52a29ee67
brandon mazey <brandon@brandons-Air.localdomain>
2023-04-26 17:20:15 -0400

    updating develop

--- notes.py
+++ notes.py
@@ -1,5 +1,12 @@
 """a simple scratchpad provided for experimentation purposes
 """
+def say_hello(name):
+    print('hello from ' + name)
+
 if __name__ == '__main__':
     # add code below ...
-    print('new dawn, new day')
+    # print('new dawn, new day')
+    # this is a comment
+    # print('hi ... my name is brandon')
+    say_hello('gabe the billionaire')
+    say_hello('arie the evil genius')
---
+++ number.py
@@ -0,0 +1,11 @@
+def add_numbers(x, y):
+    sum = x + y
+    return sum
+
+def subtract_numbers(x, y):
+    return x - y
+
+if __name__ == '__main__':
+    # result = add_numbers(3, 4)
+    # print(result)
+    print(add_numbers(3, 4))
